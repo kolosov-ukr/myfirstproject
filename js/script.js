50c3b436cee1de0ad6a3c71ad2ec5b72127b76ee
Alex <kolosov.ukr@gmail.com>
2020-05-26 12:11:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let number = 5;
+
+const obj = {
+    name: "Alex",
+    age: 25,
+    isMarried: false, 
+};
+console.log(obj.name);
+// alert('Hello');
+// const result = confirm ('Are you here ?');
+// console.log(result);
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [];
+      answers [0] = prompt('Как ваше имя ?', '');
+      answers [1] = prompt('Как ваша фамилия ?', '');
+      answers [2] = prompt('сколько вам лет ?', '');
+
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+const isChecked = true,
+      isClose = false;
+console.log(isChecked && isClose);
+
+
+
